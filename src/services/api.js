4cacde52774ba1a5320a505c4e70ec9c6ba9045f
sanjayt2706 <sanjaykundakar16@gmail.com>
2026-07-10 00:56:16 +0530

    secod commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+export default API;
 
 export const submitEnquiry = async (formData) => API.post("/api/enquiry", formData);
 
