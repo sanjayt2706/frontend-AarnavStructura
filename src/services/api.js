dddbad6c516c4645e184826279201678964cd6f8
sanjayt2706 <sanjaykundakar16@gmail.com>
2026-07-10 01:06:23 +0530

    Fix duplicate export in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
-
-
 import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export default API;
-
-export const submitEnquiry = async (formData) => API.post("/api/enquiry", formData);
+export const submitEnquiry = async (formData) =>
+  API.post("/api/enquiry", formData);
 
 export const trackPageview = async () =>
-  API.post("/api/track/pageview", { page: window.location.pathname, referrer: document.referrer }).catch(() => {});
+  API.post("/api/track/pageview", {
+    page: window.location.pathname,
+    referrer: document.referrer,
+  }).catch(() => {});
 
 export default API;
